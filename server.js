67302942b8513f6d8df1fd3d86c899772219eb6e
Jon Bloom <git@jon.tw>
2021-02-05 10:23:00 -0500

    prettier

--- server.js
+++ server.js
@@ -1,30 +1,30 @@
-const Discord = require('discord.js');
-const axios = require('axios');
-require('dotenv').config();
-const wsClinet = new (require('websocket').client);
-const moment = require('moment');
-const Sequelize = require('sequelize');
+const Discord = require("discord.js");
+const axios = require("axios");
+require("dotenv").config();
+const wsClinet = new (require("websocket").client)();
+const moment = require("moment");
+const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: 'stonks.sqlite'
+  dialect: "sqlite",
+  storage: "stonks.sqlite",
 });
 
-const Tickers = sequelize.define('tickers', {
+const Tickers = sequelize.define("tickers", {
   symbol: {
     type: Sequelize.STRING,
-    unique: true
+    unique: true,
   },
   lastPrice: {
-    type: Sequelize.FLOAT
-  }
+    type: Sequelize.FLOAT,
+  },
 });
 
-const Channels = sequelize.define('channels', {
+const Channels = sequelize.define("channels", {
   channel: {
     type: Sequelize.STRING,
-    unique: true
-  }
+    unique: true,
+  },
 });
 
 const client = new Discord.Client();
@@ -37,80 +37,92 @@ const rPrice = {};
 const lPrice = {};
 const directions = {};
 
-
 const green = "🟩";
 const red = "🟥";
 const yellow = "🟨";
 
 const PREFIX = "^^";
 
-client.on('ready', () => {
+client.on("ready", () => {
   Tickers.sync();
   Channels.sync();
   console.log(`Logged in as ${client.user.tag}!`);
 
-  wsClinet.on('connect', connection => {
-    console.log('WebSocket Client Connected');
-    Tickers.findAll().then(tickers => {
-      tickers.forEach(ticker => {
+  wsClinet.on("connect", (connection) => {
+    console.log("WebSocket Client Connected");
+    Tickers.findAll().then((tickers) => {
+      tickers.forEach((ticker) => {
         rPrice[ticker.symbol] = 0;
         lPrice[ticker.symbol] = 0;
         directions[ticker.symbol] = yellow;
         console.log(`subscribing to ${ticker.symbol}`);
-        connection.send(JSON.stringify({ 'type': 'subscribe', 'symbol': ticker.symbol }))
+        connection.send(
+          JSON.stringify({ type: "subscribe", symbol: ticker.symbol })
+        );
       });
     });
-    connection.on('message', e => {
+    connection.on("message", (e) => {
       const { data } = JSON.parse(e.utf8Data);
       if (data) {
-        data.sort((a, b) => a.t - b.t).forEach(dataPoint => {
-          rPrice[dataPoint.s] = dataPoint.p;
-          Tickers.update({
-            lastPrice: dataPoint.p
-          },
-          {
-            where: {
-              symbol: dataPoint.s
-            }
-          })
-        });
+        data
+          .sort((a, b) => a.t - b.t)
+          .forEach((dataPoint) => {
+            rPrice[dataPoint.s] = dataPoint.p;
+            Tickers.update(
+              {
+                lastPrice: dataPoint.p,
+              },
+              {
+                where: {
+                  symbol: dataPoint.s,
+                },
+              }
+            );
+          });
       }
     });
-    client.on('message', async message => {
-      if (message.content.startsWith(PREFIX) && message.author.id == "387722383567618051") {
-        const input = message.content.slice(PREFIX.length).trim().split(' ');
+    client.on("message", async (message) => {
+      if (
+        message.content.startsWith(PREFIX) &&
+        message.author.id == "387722383567618051"
+      ) {
+        const input = message.content.slice(PREFIX.length).trim().split(" ");
         const command = input.shift();
-        const commandArgs = input.join(' ');
+        const commandArgs = input.join(" ");
 
-        if (command == "add"){
+        if (command == "add") {
           const toAdd = commandArgs.trim();
           Tickers.create({
-            symbol: toAdd
-          }).then(ticker => {
-            connection.send(JSON.stringify({ 'type': 'subscribe', 'symbol': ticker.symbol }))
+            symbol: toAdd,
+          }).then((ticker) => {
+            connection.send(
+              JSON.stringify({ type: "subscribe", symbol: ticker.symbol })
+            );
             message.reply(`Symbol ${ticker.symbol} added.`);
           });
         } else if (command == "remove") {
           const toDelete = commandArgs.trim();
           Tickers.destroy({
             where: {
-              symbol: toDelete
-            }
-          }).then(ticker => {
-            connection.send(JSON.stringify({ 'type': 'unsubscribe', 'symbol': toDelete }))
+              symbol: toDelete,
+            },
+          }).then((ticker) => {
+            connection.send(
+              JSON.stringify({ type: "unsubscribe", symbol: toDelete })
+            );
             message.reply(`Symbol ${toDelete} removed.`);
           });
         } else if (command == "send") {
           Channels.create({
-            channel: message.channel.id
+            channel: message.channel.id,
           }).then(() => {
             message.reply("Subscribed to updates");
           });
         } else if (command == "unsend") {
           Channels.destroy({
             where: {
-              channel: message.channel.id
-            }
+              channel: message.channel.id,
+            },
           }).then(() => {
             message.reply("Unsubscribed to updates");
           });
@@ -121,34 +133,33 @@ client.on('ready', () => {
       const now = moment();
       let toSend = "";
       Tickers.findAll({
-        order: [
-          ['symbol', 'ASC']
-        ]
-      }).then(tickers => {
+        order: [["symbol", "ASC"]],
+      }).then((tickers) => {
         tickers.forEach((ticker, i) => {
-          if (rPrice[ticker.symbol] > lPrice[ticker.symbol]){
+          if (rPrice[ticker.symbol] > lPrice[ticker.symbol]) {
             directions[ticker.symbol] = green;
           } else if (rPrice[ticker.symbol] < lPrice[ticker.symbol]) {
             directions[ticker.symbol] = red;
           } else {
             directions[ticker.symbol] = yellow;
           }
-          toSend += `\`${directions[ticker.symbol]} ${ticker.symbol} ${formatter.format(ticker.lastPrice)}\``;
-          if (i < (tickers.length - 1)) {
+          toSend += `\`${directions[ticker.symbol]} ${
+            ticker.symbol
+          } ${formatter.format(ticker.lastPrice)}\``;
+          if (i < tickers.length - 1) {
             toSend += " ";
           }
           lPrice[ticker.symbol] = rPrice[ticker.symbol];
         });
-        Channels.findAll().then(channels => {
-          channels.forEach(channel => {
+        Channels.findAll().then((channels) => {
+          channels.forEach((channel) => {
             client.channels.cache.get(channel.channel).send(toSend);
-          })
-        })
+          });
+        });
       });
     }, 30 * 1000);
-  })
+  });
   wsClinet.connect(`wss://ws.finnhub.io?token=${process.env.FINNHUB_TOKEN}`);
-
 });
-client.on("rateLimit", e => console.log(e));
+client.on("rateLimit", (e) => console.log(e));
 client.login(process.env.DISCORD_TOKEN);
